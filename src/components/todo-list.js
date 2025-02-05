1032ee316506e6e905c2f18ea35694102a561f29
nightmare014 <timanuriev@list.ru>
2025-02-05 11:19:21 +0500

    step 11 Rest parameter for array elements

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,19 +1,21 @@
 import React from "react";
 import TodoListItem from "./todo-list-item";
-const TodoList = ({ todos}) => {
-    const elements = todos.map((item) => {
-        return (
-            <li>
-                        <TodoListItem {...item } />
 
+const TodoList = ({ todos}) => {
+
+    const elements = todos.map((item) => {
+        const { id, ...itemProps } = item;
+        return (
+            <li key = { item.id } >
+            <TodoListItem {...itemProps } />
         </li>
         );
     });
     return (
         <ul>
-                       { elements }
-
+            { elements }
         </ul>
     );
 };
+
 export default TodoList;
