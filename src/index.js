38c3dd6d27e14a8594ba096728a139a3d2d874f0
nightmare014 <timanuriev@list.ru>
2025-02-05 11:05:50 +0500

    step 6 Creating and formmating Project structure

--- src/index.js
+++ src/index.js
@@ -1,50 +1,17 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
-
-const TodoList = () => {
-    const items = ['Install React', 'Study Reac', 'Use React', 'Build React App'];
-    return (
-        <ul>
-            <li>{items[0]}</li>
-            <li>{items[1]}</li>
-            <li>{items[2]}</li>
-            <li>{items[3]}</li>
-        </ul>
-    );
-};
-
-const AppHeader = () =>{
-    return <h1>My Todo List</h1>;
-};
-
-const SearchPanel = () => {
-
-    const searchText = 'Type here to search';
-    const searchStyle = {
-        fontSize: '20px'
-    };
-
- 
-    return <input
-        style = {searchStyle}
-        placeholder= {searchText}
-        disabled={true} />;
-}
+import AppHeader from "./components/app-header";
+import SearchPanel from "./components/search-panel";
+import TodoList from "./components/todo-list";
 
 const App = () => {
-
-    const value = '<script>alert ("")</script>';
-
     return (
         <div>
-            {/*{ isLoggedIn ? null : loginBox }*/}
-            { value }
             <AppHeader />
             <SearchPanel />
             <TodoList/>
         </div>
     );
 }
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App/>);
