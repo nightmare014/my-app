6236f9ba7f5f170dd725b8fff5fa8e878cd09473
nightmare014 <timanuriev@list.ru>
2025-02-05 09:30:22 +0500

    step 3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Hello Tamirlan')
+import React from "react";
+import ReactDOM from 'react-dom/client';
+//const element = React.createElement('hi', null, 'Hello React World');
+const element = (
+    <div>
+        <h1>My Todo List</h1>
+        <input placeholder="search" />
+        <ul>
+            <li>Install React</li>
+            <li>Study React</li>
+            <li>Use React</li>
+            <li>Build React App</li>
+        </ul>
+    </div>
+    );
+    const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(element);
