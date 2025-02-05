07c7265aa1eb8f4d79760ce3ff2cf2485cd05aed
nightmare014 <timanuriev@list.ru>
2025-02-05 11:58:07 +0500

    step 17 Editing Project directories

--- src/index.js
+++ src/index.js
@@ -1,35 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppHeader from './components/app-header';
-import SearchPanel from './components/search-panel';
-import TodoList from './components/todo-list';
-import ItemStatusFilter from './components/item-status-filter';
-import './index.css';
+import App from './components/app';
 
-const App = () => {
-    const todoData = [
-        { label: 'Drink Coffee', important: false,  id: 1},
-    { label: 'React Application', important: true, id: 2 },
-    { label: 'Make notes from your stud', important: false, id: 3 },
-    { label: 'Do not drink alchohol', important: false, id: 4 },
-];
-    return (
-        <div className="todo-app">
-        <AppHeader toDo={1} done={3} />
-        <div className="top-panel d-flex">
-            <SearchPanel />
-            <ItemStatusFilter />
-        </div>
-        <TodoList todos={todoData} />
-        </div>
-    );
-}
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<App/>);
-// const todoData = [
-//     { label: 'Drink Coffee', important: false,  id: 1},
-//     { label: 'Implement React Application', important: true, id: 2 },
-//     { label: 'Make notes from your stud', important: false, id: 3 },
-//     { label: 'Do not drink alchohol', important: false, id: 4 },
-// ];
+
 
