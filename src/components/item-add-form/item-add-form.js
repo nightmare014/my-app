48490355a69893d24377202cdfd9065308a539ef
nightmare014 <timanuriev@list.ru>
2025-02-19 09:20:45 +0500

    step 33 Controlled component

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -5,15 +5,16 @@ export default class ItemAddForm extends  Component {
         label: ''
     };
  onLabelChange = (e) => {
-    // console.log('rgrggn');
-    // console.log(e.target.value)
-     this.setState({
+    this.setState({
          label: e.target.value
      });
 };
  onSubmit = (e) => {
      e.preventDefault();
     this.props.onItemAdded(this.state.label);
+    this.setState({
+        label: ''
+    });
  };
     render() {
         return (
@@ -23,8 +24,8 @@ export default class ItemAddForm extends  Component {
                 <input type={"text"}
                         className={"form-control"}
                         onChange = {this.onLabelChange}
-                        placeholder={"What need to be done"} />
-                <button
+                        placeholder={"What need to be done"}
+                        value = {this.state.label} />                <button
                     className={ "btn btn-outline-secondary" } >
                     Add Item
                 </button>
